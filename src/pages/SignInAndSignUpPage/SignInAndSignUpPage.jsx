91f5a914ee31a962986986016ca778f6808f7a43
diwao <diwao@me.com>
2021-06-13 08:21:22 +0900

    feat: adding sign-in-and-sign-out page and sigin-in component

---
+++ src/pages/SignInAndSignUpPage/SignInAndSignUpPage.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './SignInAndSignUpPage.scss';
+
+import SignIn from '../../components/SignIn/SignIn';
+
+function SignInAndSignUpPage() {
+  return (
+    <div className="sing-in-and-sign-up">
+      <SignIn />
+    </div>
+  );
+}
+
+export default SignInAndSignUpPage;
