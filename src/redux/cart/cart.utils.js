455228659a4e3073bbe7c818c5d6bc1363c173bf
diwao <diwao@me.com>
2021-07-03 13:48:35 +0900

    feat: Added clear item feature to checkout item page

--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,7 @@ export const addItemToCart = (cartItems, cartItemToAdd) => {
 
   return [...cartItems, { ...cartItemToAdd, quantity: 1 }];
 };
+
+export const clearItemFromCart = (cartItems, cartItemFromRemove) => {
+  return cartItems.filter((cartItem) => cartItem.id !== cartItemFromRemove.id);
+};
